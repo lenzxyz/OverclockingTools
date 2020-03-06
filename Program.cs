04cd938081c44ab0e7e94577423a7ca539d41f4b
wp23 <walter.pichl@hotmail.com>
2020-03-06 22:51:18 +0100

    Made cinebench downloader

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
+using System.Net;
+using System.IO.Compression;
 
 namespace OverclockTools
 {
@@ -14,10 +16,11 @@ namespace OverclockTools
         {
             Console.WriteLine("- Welcome to OverclockTools -");
             Console.WriteLine("=============================");
-            Console.WriteLine("Please select Tool to Launch ");
+            Console.WriteLine("Please select Tool to Launch (q to quit");
             Console.WriteLine("1. CoreTemp");
             Console.WriteLine("2. HW Monitor");
             Console.WriteLine("3. Prime95");
+            Console.WriteLine("4. Cinebench");
             int count = 0;
             while (count == 0)
             {
@@ -29,7 +32,7 @@ namespace OverclockTools
                 }
                 else
                 {
-                    launch(input); 
+                    launch(input);
                 }
             }
             Console.WriteLine("===============");
@@ -41,7 +44,44 @@ namespace OverclockTools
         static void launch(string toolname)
         {
             string path = Directory.GetCurrentDirectory();
-            Process.Start($@"{path}\tools\{toolname}.exe");
+            if (toolname == "cinebench")
+            {
+                try
+                {
+                    Process.Start($@"{path}\tools\{toolname}.exe");
+                }
+                catch
+                {
+                    Console.WriteLine("This tool counts as additional content and needs to be downloaded first");
+                    Console.WriteLine("Go forwad? (y for yes/n for no)");
+                    Console.Write("> ");
+                    string input = Console.ReadLine().ToLower();
+                    if (input == "y" || input == "yes")
+                    {
+                        Console.WriteLine("This could take up some time");
+                        using (var client = new WebClient())
+                        {
+                            client.DownloadFile("http://http.maxon.net/pub/cinebench/CinebenchR20.zip", "CinebenchR20.zip");
+                        }
+                        Console.WriteLine("Finished file download. Extracting file and make it ready to boot");
+                        ZipFile.ExtractToDirectory($@"{path}\CinebenchR20.zip", $@"{path}\tools");
+                        File.Delete($@"{path}\CinebenchR20.zip");
+                        Process.Start($@"{path}\tools\{toolname}.exe");
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    Process.Start($@"{path}\tools\{toolname}.exe");
+                }
+                catch
+                {
+                    Console.WriteLine($"The tool '{toolname}' could not be found");
+                }
+            }
+
         }
     }
 }
